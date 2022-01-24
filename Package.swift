dad65adcb28015b53a9270c985e4bf15cae9d344
Vadim Degterev <vadimdegterev@bttrm-vdegterev-pro16.local>
2022-01-24 15:46:22 +0200

    5.4.4 alamofire version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Alamofire",
-            url: "https://github.com/betterme-dev/AlamofireXCFramework/releases/download/1.0.0/Alamofire.xcframework.zip",
-            checksum: "79ebfb450180b2e4f68f7014554e6c4ea6907468f053fb6fff51b77925e13332"),
+            url: "https://github.com/betterme-dev/AlamofireXCFramework/releases/download/5.4.4/Alamofire.xcframework.zip",
+            checksum: "94aa0509a29c6dae58c14eb573aa01a5baff5ce63506e1ac29de3358efa95d31"),
     ]
 )
